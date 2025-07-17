dce7af7acd9cf25f21f737fba7fb8fe84400d63e
sinyu <1341156974@qq.com>
2025-07-17 10:31:42 +0800

    Initial Commit

---
+++ FirstIOSAppTest/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  FirstIOSAppTest
+//
+//  Created by 沈新宇 on 2025/7/17.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ FirstIOSAppTest/FirstIOSAppTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  FirstIOSAppTestApp.swift
+//  FirstIOSAppTest
+//
+//  Created by 沈新宇 on 2025/7/17.
+//
+
+import SwiftUI
+
+@main
+struct FirstIOSAppTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
